1e080ba890b8b388adc54b53991c1ac1ff087901
liyin <101526860+uscliyin@users.noreply.github.com>
2023-02-23 09:29:06 -0800

    Add files via upload

---
+++ HW3/HW3/Classes.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    using System;
+    using System.Collections.Generic;
+
+    // Abstraction
+    interface IPersonService
+    {
+        int CalculateAge(DateTime birthDate);
+        decimal CalculateSalary(decimal baseSalary);
+        List<Address> GetAddresses();
+    }
+
+    interface IStudentService : IPersonService
+    {
+        List<Course> GetCourses();
+        double CalculateGPA();
+    }
+
+    interface IInstructorService : IPersonService
+    {
+        Department Department { get; set; }
+        bool IsDepartmentHead { get; set; }
+        decimal CalculateBonusSalary();
+    }
+
+    interface IDepartmentService
+    {
+        Instructor Head { get; set; }
+        decimal Budget { get; set; }
+        DateTime StartDate { get; set; }
+        DateTime EndDate { get; set; }
+        List<Course> GetCourses();
+        void AddCourse(Course course);
+        void RemoveCourse(Course course);
+    }
+
+
+    class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+
+    class Person : IPersonService
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public decimal BaseSalary { get; set; }
+        private List<Address> addresses = new List<Address>();
+
+        public int CalculateAge(DateTime birthDate)
+        {
+            return DateTime.Today.Year - birthDate.Year;
+        }
+
+        public decimal CalculateSalary(decimal baseSalary)
+        {
+            if (baseSalary < 0)
+            {
+                throw new ArgumentException("Salary cannot be negative");
+            }
+            return baseSalary;
+        }
+
+        public List<Address> GetAddresses()
+        {
+            return addresses;
+        }
+
+        public void AddAddress(Address address)
+        {
+            addresses.Add(address);
+        }
+    }
+
+    class Student : Person, IStudentService
+    {
+        private List<Course> courses = new List<Course>();
+
+        public List<Course> GetCourses()
+        {
+            return courses;
+        }
+
+        public void AddCourse(Course course)
+        {
+            courses.Add(course);
+        }
+
+        public double CalculateGPA()
+        {
+            double totalGradePoints = 0;
+            double totalCredits = 0;
+            foreach (Course course in courses)
+            {
+                switch (course.Grade)
+                {
+                    case "A":
+                        totalGradePoints += 4 * course.Credits;
+                        break;
+                    case "B":
+                        totalGradePoints += 3 * course.Credits;
+                        break;
+                    case "C":
+                        totalGradePoints += 2 * course.Credits;
+                        break;
+                    case "D":
+                        totalGradePoints += 1 * course.Credits;
+                        break;
+                    default:
+                        break;
+                }
+                totalCredits += course.Credits;
+            }
+            return totalGradePoints / totalCredits;
+        }
+    }
+
+    class Instructor : Person, IInstructorService
+    {
+        public Department Department { get; set; }
+        public bool IsDepartmentHead { get; set; }
+        public DateTime JoinDate { get; set; }
+        public decimal ExperienceBonusRate { get; set; }
+
+        public decimal CalculateBonusSalary()
+        {
+            TimeSpan experience = DateTime.Now - JoinDate;
+            decimal bonus = (decimal)(experience.TotalDays / 365) * ExperienceBonusRate;
+            return bonus;
+        }
+    }
+
+    class Course
+    {
+        public string Name { get; set; }
+        public double Credits { get; set; }
+        public string Grade { get; set; }
+        private List<Student> enrolledStudents = new List<Student>();
+
+        public List<Student> EnrolledStudents
+        {
+            get { return enrolledStudents; }
+            set { enrolledStudents = value; }
+        }
+
+        public void AddStudent(Student student)
+        {
+            enrolledStudents.Add(student);
+        }
+    }
+
+    class Department : IDepartmentService
+    {
+        public Instructor Head { get; set; }
+        public decimal Budget { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        private List<Course> courses;
+
+        public Department()
+        {
+            courses = new List<Course>();
+        }
+
+        public List<Course> GetCourses()
+        {
+            return courses;
+        }
+
+        public void AddCourse(Course course)
+        {
+            courses.Add(course);
+        }
+
+        public void RemoveCourse(Course course)
+        {
+            courses.Remove(course);
+        }
+    }
+}
---
+++ HW3/HW3/Program.cs
@@ -0,0 +1,210 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+
+using System.Security.Principal;
+
+namespace HW3
+{
+    public class QuestionSolution
+    {
+        public int [] GenerateNumbers(int length)
+        {
+            int[]numbers=new int[length];
+            Random random= new Random();
+            for (int i = 0; i < length; i++)
+            {
+                numbers[i]=random.Next(0,101);
+            }
+            return numbers;
+        }
+        public void ReverseNumber(ref int[] numbers)
+        {
+            for (int i=0;i<numbers.Length/2;i++) { 
+                int firstNumber = numbers[i];
+                numbers[i] = numbers[numbers.Length - i - 1];
+                numbers[numbers.Length - i - 1] = firstNumber;
+            }
+
+        }
+        public void PrintNumber(ref int[] numbers)
+        {
+            for (int i=0;i<numbers.Length;i++)
+            {
+                Console.Write(numbers[i]+" ");
+            }
+        }
+        public void PrintFirstTenFibonacci()
+        {
+            int firstNumber = 1;
+            int secondNumber = 1;
+            int previousOneNumber = secondNumber;
+            int previousTwoNumber = firstNumber;
+            Console.Write("First 10 Fibonacci numbers are: 1, 1, ");
+            for (int i=3;i<=9;i++)
+            {
+                int currentNumber = previousOneNumber + previousTwoNumber;
+                previousTwoNumber = previousOneNumber;
+                previousOneNumber = currentNumber;
+
+                Console.Write(currentNumber+", ");
+            }
+        }
+        public int Fibonacci(int number)
+        {
+            if(number==1||number==2) { 
+            return 1;}
+            else { return Fibonacci(number - 1) + Fibonacci(number - 2); }
+
+            
+        }
+    }
+    public class Color
+    {
+
+        public int red
+        {
+            get
+            {
+                return red;
+            }
+            set
+            {
+                if (red <= 255 && red >= 0)
+                {
+                    red = value;
+                }
+            }
+        }
+
+        public int green
+        {
+            get
+            {
+                return green;
+            }
+            set
+            {
+                if (green <= 255 && green >= 0)
+                {
+                    green = value;
+                }
+            }
+        }
+        public int blue
+        {
+            get
+            {
+                return blue;
+            }
+            set
+            {
+                if (blue <= 255 && blue >= 0)
+                {
+                    blue = value;
+                }
+            }
+        }
+        public int alpha
+        {
+            get
+            {
+                return alpha;
+            }
+            set
+            {
+                if (alpha <= 255 && alpha >= 0)
+                {
+                    alpha = value;
+                }
+            }
+        }
+        public Color(int red, int green, int blue, int alpha = 255)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+
+        }
+        public decimal GetGrayScale()
+        {
+            decimal grayScale = (red + green + blue) / 3;
+            return grayScale;
+
+        }
+
+
+    }
+
+    public class Ball
+    {
+        public int size { get; set; }
+        Color color;
+        //Color color = new Color(244, 100, 13);
+        public int throwCount;
+        public Ball(int size,Color color)
+        {
+            this.size = size;
+            this.color = color;
+            this.throwCount = 0;
+        }
+
+        public void Pop()
+        {
+            size = 0;
+        }
+        public void ThrowBall()
+        {
+            if (size > 0)
+            {
+                throwCount++;
+            }
+        }
+        public int ThrowCountNumber()
+        {
+            return throwCount;
+        }
+    }
+        
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuestionSolution questionSolution= new QuestionSolution();
+
+            //Console.Write("Enter the length of an array :");
+            //int lengthArray = Int32.Parse(Console.ReadLine());
+            //int [] numbers=new int[lengthArray];
+            //numbers=questionSolution.GenerateNumbers(lengthArray);
+            //Console.WriteLine($"Randomly Create {lengthArray} numbers for you");
+            //questionSolution.PrintNumber(ref numbers);
+            //Console.WriteLine("");
+            //Console.WriteLine("After reversing these numbers");
+            //questionSolution.ReverseNumber(ref numbers);
+            //questionSolution.PrintNumber(ref numbers);
+
+
+            //questionSolution.PrintFirstTenFibonacci();
+            //Console.WriteLine(questionSolution.Fibonacci(10));
+
+
+            //Ball ballOne = new Ball(10,new Color(233,2,3));
+            //ballOne.ThrowBall();
+            //ballOne.ThrowBall();
+            //ballOne.ThrowBall();
+            //Ball ballTwo = new Ball(11, new Color(23, 2, 3));
+            //ballTwo.ThrowBall();
+            //ballTwo.Pop();
+            //ballTwo.ThrowBall();
+            //Console.WriteLine(ballOne.ThrowCountNumber());
+            //Console.WriteLine(ballTwo.ThrowCountNumber());
+
+        }
+
+    }
+}
